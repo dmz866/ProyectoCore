269453ed3e37a0c8d7f4fc81a5cab485a3784618
David Murillo <dmz866@hotmail.com>
2020-07-29 21:19:30 -0500

    first commit

---
+++ Dominio/Instructor.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Dominio
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Grado { get; set; }
+        public byte[] FotoPerfil { get; set; }
+        public ICollection<CursoInstructor> CursosLink { get; set; }
+    }
+}
---
+++ Persistencia/CursosOnlineContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Dominio;
+
+namespace Persistencia
+{
+    public class CursosOnlineContext : DbContext
+    {
+        public DbSet<Comentario> Comentario { get; set;}
+        public DbSet<Curso> Curso { get; set;}
+        public DbSet<CursoInstructor> CursoInstructor { get; set;}
+        public DbSet<Instructor> Instructor { get; set;}
+        public DbSet<Precio> Precio { get; set;}
+        
+        
+
+        public CursosOnlineContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CursoInstructor>().HasKey(ci => new { ci.InstructorId, ci.CursoId });
+        }
+    }
+}
